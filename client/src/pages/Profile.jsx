50806bdbb593c62fbe07d6656b3a24b3c59f7154
meozalat2022 <meozalat2002@hotmail.com>
2024-02-16 14:27:36 +0200

    signOut

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
 } from "../redux/user/userSlice";
 
 const Profile = () => {
@@ -98,6 +101,21 @@ const Profile = () => {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signOut");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -161,7 +179,9 @@ const Profile = () => {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          sign Out
+        </span>
       </div>
       <p className="text-red-600 mt-5">{error ? error : ""}</p>
       <p className="text-green-600 mt-5">
