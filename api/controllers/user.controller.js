c09f63759fd065ea0b1f428ddc823a96960afabd
meozalat2022 <meozalat2002@hotmail.com>
2024-02-18 11:58:31 +0200

    getting user listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../util/error.js";
 import bcrypt from "bcryptjs";
@@ -45,5 +46,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User deleted successfully");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "you can only view your own listing"));
   }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   test,
   updateUser,
   deleteUser,
+  getUserListings,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../util/verifyUser.js";
 
@@ -13,5 +14,5 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 
 router.delete("/delete/:id", verifyToken, deleteUser);
-
+router.get("/listings/:id", verifyToken, getUserListings);
 export default router;
