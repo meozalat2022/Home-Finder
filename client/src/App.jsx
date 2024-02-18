f4d1c9c479d2c14628f9ba009364c1004e64851c
meozalat2022 <meozalat2002@hotmail.com>
2024-02-18 14:29:54 +0200

    complete listing crud operations update / delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import SignIn from "./pages/SignIn";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 const App = () => {
   return (
@@ -18,6 +19,10 @@ const App = () => {
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
         <Route path="/about" element={<About />} />
         <Route path="/sign-up" element={<SignUp />} />
