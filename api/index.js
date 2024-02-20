419e45f9b5642458b891ca66654722665abed458
meozalat2022 <meozalat2002@hotmail.com>
2024-02-20 22:51:11 +0200

    pre-deploying installation

--- api/index.js
+++ api/index.js
@@ -3,13 +3,14 @@ import express from "express";
 import mongoose from "mongoose";
 
 import dotenv from "dotenv";
-
+import path from "path";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 const app = express();
 
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -30,6 +31,10 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get((req, res, next) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 //error handing middleware
 
 app.use((err, req, res, next) => {
